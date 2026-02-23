4c1bf19b43a19f105800b94cac7e7f0c05e6b8f9
NepsenX <arafatislamlam15@gmail.com>
2026-02-23 09:03:14 +0600

    Update appResolver.ts

--- src/appResolver.ts
+++ src/appResolver.ts
@@ -1,1 +1,13 @@
+const appMap = {
+  car: () => import('./products/car/frontend/public/App'),
+  ai: () => import('./products/ai/frontend/public/App'),
+}
 
+export async function loadApp(name: string) {
+  const loader = appMap[name as keyof typeof appMap]
+
+  if (!loader) throw new Error("App not found")
+
+  const module = await loader()
+  return module.default
+}
